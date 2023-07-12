17b599c6815c36f79f3cf10741d38faf660a2d91
GabrielByDuarte <gabrielfonsecaduarte533@gmail.com>
2023-07-12 07:05:12 -0300

    first commit final

--- controllers/tarefaController.js
+++ controllers/tarefaController.js
@@ -3,7 +3,7 @@ conexao = require('../models/conexao');
 
 exports.getTarefas = async (req, res) => {
     try {
-        resultado = tarefa.mostrarTarefas();
+        resultado = await tarefa.mostrarTarefas();
 
         setTimeout(function () {
             return res.render('tarefas.ejs', { resultado });
